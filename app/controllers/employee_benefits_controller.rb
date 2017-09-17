bf779b84f345d658687e324af411fdac3cf36f3c
shilpi230 <ignnovator@gmail.com>
2017-09-17 23:13:45 +0530

    feat: uploading filled docs by user

--- app/controllers/employee_benefits_controller.rb
+++ app/controllers/employee_benefits_controller.rb
@@ -1,10 +1,13 @@
 class EmployeeBenefitsController < ApplicationController
 
-  before_action :set_employee, only: :upload_template
+  before_action :set_employee, only: [:upload_template, :upload_benefit_doc]
   DOCS_PATH = File.join(Rails.root, "public", "documents")
 
   def index
     @templates = Document.templates
+    @uploaded_docs = current_employee.documents.where("document_type is NOT NULL and template =?", false)
+    @template_types = Document.templates.pluck(:document_type)
+    @all_user_docs = Document.where("document_type is NOT NULL and template =?", false)
   end
 
   def upload_template
@@ -24,6 +27,25 @@ class EmployeeBenefitsController < ApplicationController
     @document = Document.find(params[:id])
     send_file(File.join(DOCS_PATH, "#{@document.id}/#{@document.document_file_name}"))
   end
+
+  def upload_benefit_doc
+    if params[:documents]
+      params[:documents].each { |document|
+        @employee.documents.create(
+          document: document,
+          template: 0,
+          document_type: params["document"]["document_type"]
+        )
+      }
+    end
+    redirect_to employee_benefits_path
+  end
+
+  def download_benefit_doc
+    @document = Document.find(params[:id])
+    send_file(File.join(DOCS_PATH, "#{@document.id}/#{@document.document_file_name}"))
+  end
+
 
   private
     def set_employee
